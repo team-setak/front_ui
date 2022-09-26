3d7bb20658601437bc79a3e41e8f18f3131d0abb
kwlee <sksay2486@naver.com>
2022-09-26 22:14:43 +0900

    작성중

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,26 @@
 export default function Home() {
   return (
-    <div className="text-3xl font-bold underline">
-      <div>수입</div>
-      <div>지출</div>
-      <div>이번달 수익</div>
-      <div>외상 받은 돈</div>
+    <div className="flex w-auto">
+      <div className="basis-1/2 text-center m-5 w-auto">
+        <div>수입</div>
+        <table className="w-full">
+          <thead>
+            <tr>
+              <th>세탁 수익</th>
+              <th>보내야 할 돈</th>
+              <th>수선</th>
+              <th>외상</th>
+              <th>재료비</th>
+              <th>기타</th>
+              <th>오늘 수익</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      </div>
+      <div className="basis-1/4 text-center m-5">지출</div>
+      <div className="basis-1/4 text-center m-5">수익</div>
+      <div className="basis-1/4 text-center m-5">외상</div>
     </div>
   )
 }
